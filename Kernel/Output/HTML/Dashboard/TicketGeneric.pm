fbd9dbdb1f0162c8d00772e9933dbfc16b0eaabe
s7design <otrs@s7designcreative.com>
2016-02-04 13:10:11 +0100

    bug fix #11422 with selenium test

--- Kernel/Output/HTML/Dashboard/TicketGeneric.pm
+++ Kernel/Output/HTML/Dashboard/TicketGeneric.pm
@@ -12,6 +12,7 @@ use strict;
 use warnings;
 
 use Kernel::System::VariableCheck qw(:all);
+use Kernel::Language qw(Translatable);
 
 our $ObjectManagerDisabled = 1;
 
@@ -38,6 +39,9 @@ sub new {
     for my $Item (qw(SortBy OrderBy)) {
         $Self->{$Item} = $ParamObject->GetParam( Param => $Item ) || $Param{$Item};
     }
+
+    # get add filters param
+    $Self->{AddFilters} = $ParamObject->GetParam( Param => 'AddFilters' ) || $Param{AddFilters} || 0;
 
     # set filter settings
     for my $Item (qw(ColumnFilter GetColumnFilter GetColumnFilterSelect)) {
@@ -559,9 +563,17 @@ sub Run {
             }
         }
 
+        $CacheUsed = 0;
+
         # add order by parameter to the search
         if ( $Self->{OrderBy} ) {
-            $TicketSearch{OrderBy} = $Self->{OrderBy};
+            if ( $Self->{AddFilters} ) {
+                $TicketSearch{OrderBy} = $Self->{OrderBy} eq 'Up' ? 'Down' : 'Up';
+                $CacheUsed = 1;
+            }
+            else {
+                $TicketSearch{OrderBy} = $Self->{OrderBy};
+            }
         }
 
         # add process management search terms
@@ -571,7 +583,6 @@ sub Run {
             };
         }
 
-        $CacheUsed = 0;
         my @TicketIDsArray;
         if (
             !$Self->{Config}->{IsProcessWidget}
@@ -797,21 +808,27 @@ sub Run {
     # show non-labeled table headers
     my $CSS = '';
     my $OrderBy;
+    my $TitleSort;
     for my $Item (@MetaItems) {
-        $CSS = '';
-        my $Title = $Item;
-        if ( $Self->{SortBy} && ( $Self->{SortBy} eq $Item ) ) {
-            if ( $Self->{OrderBy} && ( $Self->{OrderBy} eq 'Up' ) ) {
-                $OrderBy = 'Down';
-                $CSS .= ' SortAscendingLarge';
-            }
-            else {
-                $OrderBy = 'Up';
-                $CSS .= ' SortDescendingLarge';
-            }
+        $CSS       = '';
+        $TitleSort = 0;
+        my $Title = $LayoutObject->{LanguageObject}->Translate($Item);
+        if ( $Self->{SortBy} && ( $Self->{SortBy} eq $Item ) && !$Self->{AddFilters} ) {
+            $OrderBy = ( $Self->{OrderBy} && ( $Self->{OrderBy} eq 'Up' ) ) ? 'Down' : 'Up';
+            $TitleSort = 1;
+        }
+        elsif ( $Self->{SortBy} && ( $Self->{SortBy} eq $Item ) && $Self->{AddFilters} ) {
+            $OrderBy = ( $Self->{OrderBy} && ( $Self->{OrderBy} eq 'Up' ) ) ? 'Up' : 'Down';
+            $TitleSort = 1;
+        }
 
-            # set title description
-            my $TitleDesc = $OrderBy eq 'Down' ? 'sorted ascending' : 'sorted descending';
+        # set title description
+        if ($TitleSort) {
+            $CSS
+                .= ( $Self->{OrderBy} && ( $Self->{OrderBy} eq 'Up' ) )
+                ? ' SortDescendingLarge'
+                : ' SortAscendingLarge';
+            my $TitleDesc = $OrderBy eq 'Down' ? Translatable('sorted ascending') : Translatable('sorted descending');
             $TitleDesc = $LayoutObject->{LanguageObject}->Translate($TitleDesc);
             $Title .= ', ' . $TitleDesc;
         }
@@ -865,21 +882,27 @@ sub Run {
 
         if ( $HeaderColumn !~ m{\A DynamicField_}xms ) {
 
-            $CSS = '';
-            my $Title = $HeaderColumn;
+            $CSS       = '';
+            $TitleSort = 0;
+            my $Title = $LayoutObject->{LanguageObject}->Translate($HeaderColumn);
 
-            if ( $Self->{SortBy} && ( $Self->{SortBy} eq $HeaderColumn ) ) {
-                if ( $Self->{OrderBy} && ( $Self->{OrderBy} eq 'Up' ) ) {
-                    $OrderBy = 'Down';
-                    $CSS .= ' SortAscendingLarge';
-                }
-                else {
-                    $OrderBy = 'Up';
-                    $CSS .= ' SortDescendingLarge';
-                }
+            if ( $Self->{SortBy} && ( $Self->{SortBy} eq $HeaderColumn ) && !$Self->{AddFilters} ) {
+                $OrderBy = ( $Self->{OrderBy} && ( $Self->{OrderBy} eq 'Up' ) ) ? 'Down' : 'Up';
+                $TitleSort = 1;
+            }
+            elsif ( $Self->{SortBy} && ( $Self->{SortBy} eq $HeaderColumn ) && $Self->{AddFilters} ) {
+                $OrderBy = ( $Self->{OrderBy} && ( $Self->{OrderBy} eq 'Up' ) ) ? 'Up' : 'Down';
+                $TitleSort = 1;
+            }
 
-                # add title description
-                my $TitleDesc = $OrderBy eq 'Down' ? 'sorted ascending' : 'sorted descending';
+            # set title description
+            if ($TitleSort) {
+                $CSS
+                    .= ( $Self->{OrderBy} && ( $Self->{OrderBy} eq 'Up' ) )
+                    ? ' SortDescendingLarge'
+                    : ' SortAscendingLarge';
+                my $TitleDesc
+                    = $OrderBy eq 'Down' ? Translatable('sorted ascending') : Translatable('sorted descending');
                 $TitleDesc = $LayoutObject->{LanguageObject}->Translate($TitleDesc);
                 $Title .= ', ' . $TitleDesc;
             }
@@ -930,11 +953,11 @@ sub Run {
             }
 
             my $FilterTitle     = $HeaderColumn;
-            my $FilterTitleDesc = 'filter not active';
+            my $FilterTitleDesc = Translatable('filter not active');
             if ( $Self->{GetColumnFilterSelect} && $Self->{GetColumnFilterSelect}->{$HeaderColumn} )
             {
                 $CSS .= ' FilterActive';
-                $FilterTitleDesc = 'filter active';
+                $FilterTitleDesc = Translatable('filter active');
             }
             $FilterTitleDesc = $LayoutObject->{LanguageObject}->Translate($FilterTitleDesc);
             $FilterTitle .= ', ' . $FilterTitleDesc;
@@ -2106,11 +2129,11 @@ sub _SearchParamsGet {
         },
         Watcher => {
             WatchUserIDs => [ $Self->{UserID}, ],
-            LockIDs      => $TicketSearch{LockIDs}  // undef,
+            LockIDs      => $TicketSearch{LockIDs} // undef,
         },
         Responsible => {
             ResponsibleIDs => $TicketSearch{ResponsibleIDs} // [ $Self->{UserID}, ],
-            LockIDs        => $TicketSearch{LockIDs}  // undef,
+            LockIDs        => $TicketSearch{LockIDs}        // undef,
         },
         MyQueues => {
             QueueIDs => \@MyQueues,
